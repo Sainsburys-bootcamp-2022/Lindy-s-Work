f0414f3c5d3edfb8a320ead4661aea2134c45ca9
LindyWatts <104976055+LindyWatts@users.noreply.github.com>
2022-05-18 14:42:09 +0100

    Add files via upload

--- academy.js
+++ academy.js
@@ -1,54 +1,88 @@
 // Make your changes to store and update game state in this file
 
-let board = [[null, null, null][null, null, null][null, null, null]]
+let board =[
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+let gameOver = false
+let noughtsTurn = true
+console.log("Noughts Turn")
 
-function getNewBoard() {
-    return[null, null, null][null, null, null][null, null, null]
-}
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 // Take the row and column number between 0 and 2
 // (inclusive) and update the game state.
 function takeTurn(row, column) {
-        console.log(board[row][column])
-        const currentsquare = board[row][column]
-        for (const rowIndex in board) {
-            for (const columnIndex in rowIndex) {
-                if (board [rowIndex][columnIndex] === null) {
-            }
-        }
-    }
-}
+
+    if (!gameOver) {
     console.log("takeTurn was called with row: " + row + ", column:" + column);
     console.log(`takeTurn was called with row: ${row}, column: ${column}`);
-    function positionClick() {
-        console.log("drawBoard was called");
-        return (board)
-    }
 
+    if (board[row][column] === null && noughtsTurn) {
+        board[row][column] = "nought"
+        noughtsTurn = false
+        console.log(board)
+        console.log("Crosses Turn")
+    } else if (board[row][column] === null){
+        board[row][column] = "cross"
+        noughtsTurn = true
+        console.log("Noughts Turn")
+    } else {
+        console.log("Space already taken")
+        console.log(board)
+    }
+}
+}
 // Return either "noughts", "crosses" or "nobody" if the game is over.
 // Otherwise return null to continue playing.
 function checkWinner() {
+    // if checkWinner === winningConditions ()
+//         console.log("winner")
+    
+// }
+    // let roundWon = false;                 
+    //   for (let i = 0; i <= 7; i++) {
+    //      const winner = winningConditions[i];
+    //       let a = board[winner[0]];
+    //       let b = board[winner[1]];
+    //       let c = board[winner[2]];
+    //       if (a === '' || b === '' || c === '') {
+    //            continue;
+    //        }
+    //        if (a === b && b === c) {
+    //           roundWon = true;
+    //            break
+    //        }
+    //    }
     console.log("checkWinner was called");
     return null;
 }
 
 // Set the game state back to its original state to play another game.
-function resetGame() {
-    function getNewBoard()
-    //function clearBoard() {
-        //for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-            //for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
-                //document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerHTML = ""
-            //}
-       // }
-    //}    
-    console.log(board);
+function resetGame(clearBoard) {
+      console.log("resetGame was called");   
+      board = 
+      [[null, null, null],
+      [null, null, null],
+      [null, null, null]]
 }
+
 
 // Return the current board state with either a "nought" or a "cross" in
 // each position. Put a null in a position that hasn't been played yet.
 function getBoard() {
     console.log("getBoard was called");
-    return [[null, null, null], [null, null, null], [null, null, null]];
+    return board;
 }
 
 if (typeof exports === 'object') {
